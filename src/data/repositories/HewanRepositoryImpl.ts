054c4c534fbd08d298e38ad2c1f4699fdb9a7329
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-06-05 10:48:22 +0700

    menambahkan method create, update dan delete pada HewanRepository

--- src/data/repositories/HewanRepositoryImpl.ts
+++ src/data/repositories/HewanRepositoryImpl.ts
@@ -11,6 +11,17 @@ export class HewanRepositoryImpl implements IHewanRepository{
         const response =await apiClient.get<APIResponse<Hewan>>(`/hewan/${id}`);
         return response.data;
     }
-    
+    async create(hewan:Omit<Hewan,'id'>):Promise<APIResponse<Hewan>>{
+        const response =await apiClient.post<APIResponse<Hewan>>('/hewan', hewan);
+        return response.data;
+    }
+    async update(id:number, hewan:Partial<Hewan>):Promise<APIResponse<Hewan>>{
+        const response =await apiClient.put<APIResponse<Hewan>>(`/hewan/${id}`, hewan);
+        return response.data;
+    }
+    async delete(id:number):Promise<APIResponse<{message:string}>>{
+        const response =await apiClient.delete<APIResponse<{message:string}>>(`/hewan/${id}`);
+        return response.data;
+    }
 
 }

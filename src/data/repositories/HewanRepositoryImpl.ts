81dd904789a1b55c9fcc2996ee5bc44188b640d3
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-06-05 10:46:30 +0700

    menambahkan implementasi HewanRepository

---
+++ src/data/repositories/HewanRepositoryImpl.ts
@@ -0,0 +1,8 @@
+import {IHewanRepository} from '../../domain/repositories/IHewanRepository';
+import apiClient from '../api/apiClient';
+import{Hewan, APIResponse} from '../../domain/entities/Hewan';
+
+export class HewanRepositoryImpl implements IHewanRepository{
+   
+
+}

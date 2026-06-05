ecd3376d48dc66cb63d63a4742c1aff575ed8e4f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-06-05 10:47:21 +0700

    menambahkan method getAll pada HewanRepository

--- src/data/repositories/HewanRepositoryImpl.ts
+++ src/data/repositories/HewanRepositoryImpl.ts
@@ -3,6 +3,10 @@ import apiClient from '../api/apiClient';
 import{Hewan, APIResponse} from '../../domain/entities/Hewan';
 
 export class HewanRepositoryImpl implements IHewanRepository{
-   
+    async getAll():Promise<APIResponse<Hewan[]>>{
+        const response =await apiClient.get<APIResponse<Hewan[]>>('/hewan');
+        return response.data;
+    }
+    
 
 }
